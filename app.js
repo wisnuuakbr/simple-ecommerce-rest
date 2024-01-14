601571dce3a5caef5114d026142cafcdaab5e3da
wisnuuakbr <wisnuaero3@gmail.com>
2024-01-14 08:57:09 +0700

    :tada: update apps.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const path = require('path');
+const userRouter = require('./routers/user')
+const itemRouter = require('./routers/item')
+const cartRouter = require('./routers/cart')
+const orderRouter = require('./routers/order')
 require('./db/mongoose')
 
 const port = process.env.PORT
@@ -6,6 +11,18 @@ const port = process.env.PORT
 const app = express()
 
 app.use(express.json())
+app.use(userRouter)
+app.use(itemRouter)
+app.use(cartRouter)
+app.use(orderRouter)
+
+// connect to frontend
+// const publicDirectory = path.join(__dirname, '../public')
+// app.use(express.static(publicDirectory))
+
+// app.get('/', (req, res) => {
+//     res.sendFile('index.html')
+// })
 
 app.listen(port, () => {
     console.log('server listening on port ' + port)
